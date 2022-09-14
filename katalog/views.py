0cb2a6534dd1cb4214c11ad1abb61a9840848680
pritaelisabethlauratarigan <pritatarigan01@gmail.com>
2022-09-14 15:06:43 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Prita Elisabeth Laura Tarigan',
+        'npm': '2106751190'
+    }
+    return render(request, "katalog.html", context)
