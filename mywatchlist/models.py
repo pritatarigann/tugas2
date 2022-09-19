da73d4edfc54c13caedf76834fff881bab119e21
pritaelisabethlauratarigan <pritatarigan01@gmail.com>
2022-09-20 03:59:18 +0700

    commit2

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class MyWatchList(models.Model):
     watched = models.BooleanField() 
-    title = models.CharField(max_length=255)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    title = models.TextField()
+    rating = models.IntegerField()
     release_date = models.DateField()
     review = models.TextField()
