79a2f607d63005bde500474485f6f1d601e4e7a9
pritaelisabethlauratarigan <pritatarigan01@gmail.com>
2022-09-28 01:31:25 +0700

    2nd commit

--- todolist/views.py
+++ todolist/views.py
@@ -14,50 +14,50 @@ from todolist.task_form import TaskForm
 from todolist.models import Task
 import pytz
 
-# Fungsi untuk memproses registrasi user
+# user registration
 def register(request):
     form = UserCreationForm()
 
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save() # Menyimpan data akun ke database
-            messages.success(request, 'Akun telah berhasil dibuat!')
+            form.save() # add to database
+            messages.success(request, 'Account has been created successfully!')
             return redirect('todolist:login')
     
     context = {'form':form}
     return render(request, 'register.html', context)
 
-# Fungsi untuk memproses aktivitas login 
+# user login
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user) # Melakukan login terlebih dahulu
+            login(request, user) # login
             response = HttpResponseRedirect(reverse("todolist:show_todolist")) # Membuat response
             timezone = pytz.timezone('Asia/Jakarta')
-            response.set_cookie('last_login', str(datetime.datetime.now(tz = timezone))) # Membuat cookie last_login dan menambahkannya ke dalam response
+            response.set_cookie('last_login', str(datetime.datetime.now(tz = timezone))) # create cookie last_login 
             return response
         else:
             messages.info(request, 'Username atau Password salah!')
     context = {}
     return render(request, 'login.html', context)
 
-# Fungsi untuk memproses aktivitas logout
+# user logout
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('todolist:login'))
     response.delete_cookie('last_login')
     return response
 
-@login_required(login_url='/todolist/login/') # Merestriksi akses halaman todolist
-# Fungsi untuk menampilkan todolist user
+@login_required(login_url='/todolist/login/') # restrict
+# todolist 
 def show_todolist(request):
     username = request.user.username
     user_id = request.user.id
-    data_todolist = Task.objects.filter(user_id=user_id) # Menyimpan hasil query dari data pada Task dengan user_id tertentu
+    data_todolist = Task.objects.filter(user_id=user_id) # save data
 
     context = { 
         "username": username,
@@ -65,18 +65,18 @@ def show_todolist(request):
     }
     return render(request, "todolist.html", context)
 
-@login_required(login_url='/todolist/login/') # Merestriksi akses halaman create-task
-# Fungsi untuk memproses pembuatan task
+@login_required(login_url='/todolist/login/') # restrict
+# add tasks
 def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
-        if form.is_valid(): # Kondisi data pada field valid
+        if form.is_valid(): # requirements fulfilled
             task = Task(
                 user = request.user,
                 title = form.cleaned_data['judul'], 
                 description = form.cleaned_data['deskripsi'],
             )
-            task.save() # Menyimpan task ke database
+            task.save() # save task
             return HttpResponseRedirect(reverse("todolist:show_todolist"))
     else:
         form = TaskForm()
@@ -85,17 +85,17 @@ def create_task(request):
     return render(request, "create_task.html", context)
 
 @login_required(login_url='/todolist/login/')
-# Fungsi untuk menghapus task
+# delete task
 def delete_task(request, id):
-	deleted_task = Task.objects.get(id=id) # Mengambil data task yang ingin dihapus sesuai id
-	deleted_task.delete() # Menghapus task 
+	deleted_task = Task.objects.get(id=id) # get task
+	deleted_task.delete() # delete task 
 
 	return HttpResponseRedirect("/todolist")
 
 #resource: https://www.w3schools.com/django/django_delete_record.php
 
 @login_required(login_url='/todolist/login/')
-# Fungsi untuk memperbarui status task
+# update tasks state
 def update_state(request, id):
     updated_task = Task.objects.get(id=id)
 
@@ -104,5 +104,5 @@ def update_state(request, id):
     else:
         updated_task.is_finished = True
     
-    updated_task.save() # Menyimpan pembaruan ke database
+    updated_task.save() # save
     return HttpResponseRedirect("/todolist")
